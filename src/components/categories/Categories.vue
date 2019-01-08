96ffe46f49951156157a75f38924d8c2b70b0e23
dongsisi <1713420205@qq.com>
2019-01-08 21:44:07 +0800

    商品管理-商品分类-添加分类功能完成

--- src/components/categories/Categories.vue
+++ src/components/categories/Categories.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="categories">
+    <!-- 添加分类组件 -->
+    <el-button type="success" plain @click="showAddCateDialog">添加分类</el-button>
     <!-- 表格组件 -->
     <el-table :data="cateList" stripe style="width: 100%" v-loading="loading">
     <el-table-tree-column prop="cat_name" label="分类名称" width="220" tree-key="cat_id" level-key="cat_level" parent-key="cat_pid" :indent-size="30"></el-table-tree-column>
@@ -16,7 +18,6 @@
       </template>
     </el-table-column>
   </el-table>
-  <!-- 分页组件 -->
    <!-- 分页组件： -->
   <el-pagination
     background
@@ -27,6 +28,26 @@
     @current-change="changePage"
   >
   </el-pagination>
+  <!-- 添加分类对话框组件 -->
+  <el-dialog title="添加分类" :visible.sync="isShowAddCateDialog">
+  <el-form :model="AddCateForm" label-width="100px">
+    <el-form-item label="分类名称" >
+      <el-input v-model="AddCateForm.cat_name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="父级名称" >
+      <el-cascader  v-model="AddCateForm.catPidArr"
+          :options="AddCateList" change-on-select
+          :props="{
+            label: 'cat_name',
+            value: 'cat_id'
+          }"></el-cascader>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="isShowAddCateDialog = false">取 消</el-button>
+    <el-button type="primary" @click="addCate">确 定</el-button>
+  </div>
+</el-dialog>
   </div>
 </template>
 
@@ -42,11 +63,21 @@ export default {
       // 每页大小
       pagesize: 5,
       //加载中效果
-      loading:false
+      loading:false,
+      //添加分类数据
+      AddCateForm:{
+        //级联选择器的数据
+        catPidArr:[],
+        cat_name:''
+      },
+      //添加分类展示与隐藏
+      isShowAddCateDialog:false,
+      AddCateList:[],
     }
   },
   created(){
     this.getCateList()
+    this.getAddCateList()
   },
   methods:{
     async getCateList(curPage=1){
@@ -68,7 +99,43 @@ export default {
     //切换分页
      changePage (curPage) {
       this.getCateList(curPage)
+    },
+    //展示添加分类对话框
+    showAddCateDialog(){
+      this.isShowAddCateDialog = true
+    },
+    //添加分类列表（获取级联选择器数据一级二级菜单数据）功能
+    async getAddCateList(){
+      // console.log("ee")
+      const res = await this.$http.get('/categories',{
+        params:{
+          type:2
+        }
+      })
+      this.AddCateList = res.data.data
+    },
+    //添加分类
+    async addCate(){
+      const {cat_name,catPidArr} = this.AddCateForm
+      // console.log(cat_name, catPidArr[catPidArr.length - 1], catPidArr.length)
+
+      const res = await this.$http.post('/categories',{
+        cat_pid:catPidArr[catPidArr.length-1],
+        cat_name,
+        cat_level:catPidArr.length,
+      })
+      // console.log(res)
+      //关闭对话框
+      this.isShowAddCateDialog = false
+      //提示消息
+      this.$message({
+        type:"success",
+        message:res.data.meta.msg
+      })
+      //刷新列表
+      this.getCateList()
     }
+
   }
 }
 </script>
